b1d2d5af778390831c4e3672e13720685ae84693
glazydonut <l.indra@student.unsw.edu.au>
2022-12-12 18:38:24 +1100

    Client Removed

--- client/src/redux/Store.js
+++
@@ -1,5 +0,0 @@
-import thunk from 'redux-thunk'
-import reducers from '../reducers/'
-import {  createStore, applyMiddleware, compose } from 'redux'
-
-export const store = createStore(reducers, compose(applyMiddleware(thunk)))
